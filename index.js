45810501fec0e34f74988afeda07ba36f05e701d
alx-yng <alexyoung0119@gmail.com>
2024-04-19 18:44:03 -0500

    add db connection, remove mongoose

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8081;
+const client = new MongoClient(process.env.DB_HOST);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.get("/article", async (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
